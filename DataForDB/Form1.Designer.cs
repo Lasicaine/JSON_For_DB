98c2083178503c88566802167ac810366e62e934
Suomaa <a.antomonov@mail.ru>
2016-08-11 16:55:46 +0300

    Added option "number of records"

--- DataForDB/Form1.Designer.cs
+++ DataForDB/Form1.Designer.cs
@@ -29,7 +29,11 @@
         private void InitializeComponent()
         {
             this.ResultTextBox = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.labelNumberRec = new System.Windows.Forms.Label();
+            this.numericUpDownNumRec = new System.Windows.Forms.NumericUpDown();
+            this.progressBarGeneration = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownNumRec)).BeginInit();
             this.SuspendLayout();
             // 
             // ResultTextBox
@@ -37,35 +41,82 @@
             this.ResultTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.ResultTextBox.Location = new System.Drawing.Point(12, 40);
+            this.ResultTextBox.Location = new System.Drawing.Point(12, 45);
             this.ResultTextBox.Multiline = true;
             this.ResultTextBox.Name = "ResultTextBox";
             this.ResultTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.ResultTextBox.Size = new System.Drawing.Size(640, 510);
+            this.ResultTextBox.Size = new System.Drawing.Size(640, 495);
             this.ResultTextBox.TabIndex = 0;
             // 
-            // button1
+            // buttonGenerate
             // 
-            this.button1.Location = new System.Drawing.Point(12, 8);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Generate";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonGenerate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonGenerate.Location = new System.Drawing.Point(577, 11);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(75, 23);
+            this.buttonGenerate.TabIndex = 1;
+            this.buttonGenerate.Text = "Generate";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // labelNumberRec
+            // 
+            this.labelNumberRec.AutoSize = true;
+            this.labelNumberRec.Location = new System.Drawing.Point(9, 16);
+            this.labelNumberRec.Name = "labelNumberRec";
+            this.labelNumberRec.Size = new System.Drawing.Size(97, 13);
+            this.labelNumberRec.TabIndex = 2;
+            this.labelNumberRec.Text = "Number of records:";
+            // 
+            // numericUpDownNumRec
+            // 
+            this.numericUpDownNumRec.Location = new System.Drawing.Point(112, 14);
+            this.numericUpDownNumRec.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDownNumRec.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownNumRec.Name = "numericUpDownNumRec";
+            this.numericUpDownNumRec.Size = new System.Drawing.Size(66, 20);
+            this.numericUpDownNumRec.TabIndex = 3;
+            this.numericUpDownNumRec.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownNumRec.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.numericUpDownNumRec_KeyPress);
+            // 
+            // progressBarGeneration
+            // 
+            this.progressBarGeneration.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBarGeneration.Location = new System.Drawing.Point(12, 553);
+            this.progressBarGeneration.Name = "progressBarGeneration";
+            this.progressBarGeneration.Size = new System.Drawing.Size(640, 19);
+            this.progressBarGeneration.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBarGeneration.TabIndex = 4;
             // 
             // DataForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(662, 561);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(662, 584);
+            this.Controls.Add(this.progressBarGeneration);
+            this.Controls.Add(this.numericUpDownNumRec);
+            this.Controls.Add(this.labelNumberRec);
+            this.Controls.Add(this.buttonGenerate);
             this.Controls.Add(this.ResultTextBox);
             this.Cursor = System.Windows.Forms.Cursors.Hand;
             this.MinimumSize = new System.Drawing.Size(300, 300);
             this.Name = "DataForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DataForDB";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownNumRec)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -74,7 +125,10 @@
         #endregion
 
         private System.Windows.Forms.TextBox ResultTextBox;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.Label labelNumberRec;
+        private System.Windows.Forms.NumericUpDown numericUpDownNumRec;
+        private System.Windows.Forms.ProgressBar progressBarGeneration;
     }
 }
 
--- DataForDB/Form1.cs
+++ DataForDB/Form1.cs
@@ -17,18 +17,33 @@ namespace DataForDB
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void buttonGenerate_Click(object sender, EventArgs e)
         {
+            ResultTextBox.Text = "";
+            progressBarGeneration.Maximum = Convert.ToInt32(numericUpDownNumRec.Value);
+            progressBarGeneration.Value = 0;
+            progressBarGeneration.Step = 1;
             string beg = "{\"Addresses\":[{\"Type\":2,\"Value\":\"rambler.ru\"}";
             string str1 = ",{\"Type\":2,\"Value\":\"rambler";
             string str2 = ".ru\"}";
-          
-            for (int i = 1; i <= 198; i++)
+            StringBuilder res = new StringBuilder();
+
+            for (int i = 1; i <= numericUpDownNumRec.Value; i++)
             {
-                beg = beg + str1 + i + str2;
+                res.Append(beg).Append(str1).Append(i).Append(str2);
+                progressBarGeneration.PerformStep();
+                //progressBarGeneration.Value++;
             }
-            string ed = beg + "]}";
-            ResultTextBox.Text = ed;
+
+            StringBuilder ed = new StringBuilder();
+            ed.Append(res + "]}");
+            string resstr = ed.ToString();
+            ResultTextBox.Text = resstr;
+        }
+        private void numericUpDownNumRec_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < '0') || (e.KeyChar > '9'))
+                e.Handled = true;
         }
     }
 }
